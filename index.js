7258c3e5abad6a6ecf7be34c225ff3c56462604f
Akshit Sangwan <85335888+akshitsangwan@users.noreply.github.com>
2023-06-02 19:44:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+let typeData = new Typed(".role", {
+  strings: [
+      "Full Stack Developer",
+      "Web Developer",
+      "UI-UX Designer",
+      "Backend Developer",
+      "Coder",
+  ],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1000,
+});
+
+let year = new Date().getFullYear();
+document.getElementById("year").innerHTML = year;
+
+let education = document.getElementById("education");
+let work = document.getElementById("work");
+let educationheader = document.getElementById("educationheader");
+let workheader = document.getElementById("workheader");
+workheader.style.color = "var(--text-color)";
+educationheader.style.color = "var(--first-color)";
+
+educationheader.addEventListener("click", () => {
+  let condition1 = work.classList.contains("qualification-inactive");
+  if (!condition1) {
+    education.classList.remove("qualification-inactive");
+    work.classList.add("qualification-inactive");
+    workheader.style.color = "var(--text-color)";
+    educationheader.style.color = "var(--first-color)";
+  }
+});
+workheader.addEventListener("click", () => {
+  let condition2 = education.classList.contains("qualification-inactive");
+  if (!condition2) {
+    work.classList.remove("qualification-inactive");
+    education.classList.add("qualification-inactive");
+    educationheader.style.color = "var(--text-color)";
+    workheader.style.color = "var(--first-color)";
+  }
+});
+
